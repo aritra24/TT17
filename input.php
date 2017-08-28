61f76d765c8af0c28051c7ecdf9315946b6f746b
rakshit98 <31076384+rakshit98@users.noreply.github.com>
2017-08-29 01:38:03 +0530

    Add files via upload

--- input.php
+++ input.php
@@ -1,8 +1,9 @@
 <?php 
 include 'database.inc.php';
+session_start();
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$username = $password = $username_error = $password_error = '';
-		$username_desk = $password_desk = $username_desk_error = $password_desk_error = '';
+		$user = $pass = $user_error = $pass_error = '';
 		
 		if (isset($_POST['username'])) {
 			if(empty($_POST['username'])) {
@@ -22,73 +23,85 @@ include 'database.inc.php';
 				$password = test_input($_POST['password']);
 			}
 		}
-		if (isset($_POST['username_desk'])) {
-			if(empty($_POST['username_desk'])) {
-				$username_desk_error = 'Username is required';
+		if (isset($_POST['user'])) {
+			if(empty($_POST['user'])) {
+				$user_error = 'Username is required';
 			} else {
-				if($_POST['username_desk'] == test_input($_POST['username_desk'])) {
-					$username_desk = test_input($_POST['username_desk']);
+				if($_POST['user'] == test_input($_POST['user'])) {
+					$user = test_input($_POST['user']);
 				} else {
-					$username_desk_error = 'Use a different username';
+					$user_error = 'Use a different username';
 				}
 			}
 		}
-		if (isset($_POST['password_desk'])) {
-			if(empty($_POST['password_desk'])) {
-				$password_desk_error = 'Password is required';
+		if (isset($_POST['pass'])) {
+			if(empty($_POST['pass'])) {
+				$pass_error = 'Password is required';
 			} else {
-				$password_desk = test_input($_POST['password_desk']);
+				$pass = test_input($_POST['pass']);
 			}
 		}
-             
-		if($username_error == '' && $password_error == '' && $username_desk_error == '' && $password_desk_error == '') {
+
+		if($username_error == '' && $password_error == '' && $user_error == '' && $pass_error == '') {
 			$select_statement = $conn -> prepare('SELECT * FROM login WHERE UID = ?');
 			$select_statement -> bind_param('s', $username);
 			$select_statement -> execute();
 			$result = $select_statement -> get_result();
 			$row = $result -> fetch_assoc();
+			echo $row['UID'];
 
 			$select_statement_desk = $conn -> prepare('SELECT * FROM loginDesk WHERE UID = ?');
-			$select_statement_desk -> bind_param('s', $username_desk);
+			$select_statement_desk -> bind_param('s', $user);
 			$select_statement_desk -> execute();
 			$result_desk = $select_statement_desk -> get_result();
 			$row_desk = $result_desk -> fetch_assoc();
+			echo $row_desk['UID'];
 
+			$query = "SELECT * FROM time WHERE UID = '$user';";
+			mysqli_query($conn,$query);
+			$row_log = mysql_fetch_array($query);
+			}
 			if($row == NULL && $row_desk == NULL) {
 				$mssg = 'Users not found';
 				$username = '';
 				$password = '';
-				$username_desk = '';
-				$password_desk = '';
-				//header('Location:loginform.php');
+				$user = '';
+				$pass = '';
+				header('Location:loginform.php');
 			} else if($row == NULL) {
 				$mssg = $username . ' not found';
 				$username = '';
 				$password = '';
-				$username_desk=$_SESSION['username_desk'];
-				//header('Location:loginform.php');
+				$user = $_SESSION['user'];
+				header('Location:loginform.php');
 			} else if($row_desk == NULL) {
-				$mssg = $username_desk . ' not found';
-				$username_desk = '';
-				$password_desk = '';
+				$mssg = $user . ' not found';
+				$user = '';
+				$pass = '';
 				$username = $_SESSION['username'];
-				//header('Location:loginform.php');
-			} else if(($password) == $row['PASS'] && ($password_desk) == $row_desk['PASS']) {
+				header('Location:loginform.php');
+			} else if($row_log['logged'] == '1') {
+				header('Location:loginform.php');
+			}
+				else if(($password) == $row['PASS'] && ($pass) == $row_desk['PASS']) {
 				$_SESSION['user'] = $row['UID'];
-				$sql = "INSERT INTO info_login(UID,login,intime) VALUES ('$username_desk','1',now());";
-				header('Location:register.html');
+				$sql = "INSERT INTO time(UID,logged,intime,sysin) VALUES ('$user','1',now(),now());";
+				mysqli_query($conn,$sql);
+				$_SESSION['UID'] = $user;
+                header('Location: register.php');
 			} else {
 				$mssg = 'Incorrect Password';
 				if(($password) != $row['PASS'])
 					$password = '';
-				if(($password_desk) != $row_desk['PASS'])
-					$password_desk = '';
+				if(($pass) != $row_desk['PASS'])
+					$pass = '';
 				header('Location:loginform.php');
 			}
 			$select_statement -> close();
 			$select_statement_desk -> close();
 		}
-	}
+	
+
 	function test_input($data) {
 		$data = trim($data);
 		$data = stripslashes($data);
